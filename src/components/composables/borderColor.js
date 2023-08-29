d709e5ab1db25a44004072c0449d3a04e916d7d2
mthdht <mthdht@gmail.com>
2023-08-29 13:52:26 +0200

    create border color composable

---
+++ src/components/composables/borderColor.js
@@ -0,0 +1,44 @@
+export const backgroundColor = {
+  gray: {
+    fill: 'border-slate-600',
+    light: 'border-slate-300',
+    outlined: 'border-slate-500',
+    text: 'border-slate-500',
+    none: '',
+  },
+  red: {
+    fill: 'border-red-600',
+    light: 'border-red-300',
+    outlined: 'border-red-500',
+    text: 'border-red-500',
+    none: '',
+  },
+  orange: {
+    fill: 'border-orange-600',
+    light: 'border-orange-300',
+    outlined: 'border-orange-500',
+    text: 'border-orange-500',
+    none: '',
+  },
+  yellow: {
+    fill: 'border-yellow-600',
+    light: 'border-yellow-300',
+    outlined: 'border-yellow-500',
+    text: 'border-yellow-500',
+    none: '',
+  },
+  green: {
+    fill: 'border-emerald-600',
+    light: 'border-emerald-300',
+    outlined: 'border-emerald-500',
+    text: 'border-emerald-500',
+    none: '',
+  },
+  blue: {
+    fill: 'border-sky-600',
+    light: 'border-sky-300',
+    outlined: 'border-sky-500',
+    text: 'border-sky-500',
+    none: '',
+  },
+};
