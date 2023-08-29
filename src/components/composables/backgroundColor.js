5c0f00a5ab21860b5a35814e7d5c20ba4a6113de
mthdht <mthdht@gmail.com>
2023-08-29 13:47:39 +0200

    create background color composable

--- src/components/composables/backgroundColor.js
+++ src/components/composables/backgroundColor.js
@@ -13,5 +13,32 @@ export const backgroundColor = {
     text: 'text-red-600',
     none: '',
   },
-  orange: {},
+  orange: {
+    fill: 'bg-orange-500 text-white',
+    light: 'bg-orange-100 text-orange-600',
+    outlined: 'text-orange-500 border-2 border-orange-500',
+    text: 'text-orange-600',
+    none: '',
+  },
+  yellow: {
+    fill: 'bg-yellow-500 text-white',
+    light: 'bg-yellow-100 text-yellow-600',
+    outlined: 'text-yellow-500 border-2 border-yellow-500',
+    text: 'text-yellow-600',
+    none: '',
+  },
+  green: {
+    fill: 'bg-emerald-500 text-white',
+    light: 'bg-emerald-100 text-emerald-600',
+    outlined: 'text-emerald-500 border-2 border-emerald-500',
+    text: 'text-emerald-600',
+    none: '',
+  },
+  blue: {
+    fill: 'bg-sky-500 text-white',
+    light: 'bg-sky-100 text-sky-600',
+    outlined: 'text-sky-500 border-2 border-sky-500',
+    text: 'text-sky-600',
+    none: '',
+  },
 };
