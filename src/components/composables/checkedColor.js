764eb7d8208fd16f84019dcc2870457940793de6
mthdht <mthdht@gmail.com>
2023-08-29 14:06:06 +0200

    create checked color composable

---
+++ src/components/composables/checkedColor.js
@@ -0,0 +1,44 @@
+export const checkedColor = {
+  gray: {
+    fill: 'bg-slate-500 checked:bg-slate-700 checked:hover:bg-slate-800',
+    light: 'bg-slate-200 checked:bg-slate-500 checked:hover:bg-slate600',
+    outlined: 'checked:bg-slate-500 checked:hover:bg-slate-600',
+    text: '',
+    none: '',
+  },
+  red: {
+    fill: 'bg-red-500 checked:bg-red-700 checked:hover:bg-red-800',
+    light: 'bg-red-200 checked:bg-red-500 checked:hover:bg-red600',
+    outlined: 'checked:bg-red-500 checked:hover:bg-red-600',
+    text: '',
+    none: '',
+  },
+  orange: {
+    fill: 'bg-orange-500 checked:bg-orange-700 checked:hover:bg-orange-800',
+    light: 'bg-orange-200 checked:bg-orange-500 checked:hover:bg-orange600',
+    outlined: 'checked:bg-orange-500 checked:hover:bg-orange-600',
+    text: '',
+    none: '',
+  },
+  yellow: {
+    fill: 'bg-yellow-500 checked:bg-yellow-700 checked:hover:bg-yellow-800',
+    light: 'bg-yellow-200 checked:bg-yellow-500 checked:hover:bg-yellow600',
+    outlined: 'checked:bg-yellow-500 checked:hover:bg-yellow-600',
+    text: '',
+    none: '',
+  },
+  green: {
+    fill: 'bg-emerald-500 checked:bg-emerald-700 checked:hover:bg-emerald-800',
+    light: 'bg-emerald-200 checked:bg-emerald-500 checked:hover:bg-emerald600',
+    outlined: 'checked:bg-emerald-500 checked:hover:bg-emerald-600',
+    text: '',
+    none: '',
+  },
+  blue: {
+    fill: 'bg-sky-500 checked:bg-sky-700 checked:hover:bg-sky-800',
+    light: 'bg-sky-200 checked:bg-sky-500 checked:hover:bg-sky600',
+    outlined: 'checked:bg-sky-500 checked:hover:bg-sky-600',
+    text: '',
+    none: '',
+  },
+};
