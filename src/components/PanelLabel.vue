577162f9d9975a0deee599bd188d287cc550ecc2
mthdht <mthdht@gmail.com>
2025-05-07 07:51:30 +0200

    made toggle icon to be as prop

--- src/components/PanelLabel.vue
+++ src/components/PanelLabel.vue
@@ -1,29 +1,47 @@
 <template>
-    <div class="l-panel-label cursor-pointer"  @click="panel.toggle" :class="[pharos.theme.padding]">
+    <div class="l-panel-label cursor-pointer flex items-center justify-between"  @click="panel.toggle" :class="[pharos.theme.padding]">
         <slot>trigger</slot>
-        <component :is="openIcon"></component>
+        <Transition mode="out-in">
+            <component :is="icon"></component>
+        </Transition>
     </div>
 </template>
 
 <script setup>
-import { inject, computed, ref } from 'vue'
+import { inject, reactive, computed } from 'vue'
 
 const props = defineProps({
     icons: {
         type: Array,
-        default: ['Minus', 'Close']
+        default: ['Minus', 'Plus']
     }
 })
 const pharos = inject('pharos')
 const panel = inject('panel')
 
-const openIcon = ref()
+const toggleIcons = reactive({
+    open: null,
+    close: null
+})
+
+const icon = computed(() => {
+    return panel.isOpen.value ? toggleIcons.open : toggleIcons.close
+})
 
 import('lucide-vue-next').then((icons) => {
-    openIcon.value = icons[props.icons[0]];
+    toggleIcons.open = icons[props.icons[0]];
+    toggleIcons.close = icons[props.icons[1]];
 })
 </script>
 
 <style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s ease;
+}
 
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 </style>
