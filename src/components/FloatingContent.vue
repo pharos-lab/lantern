bb8ffccc24befa656087ea23ff222acc4a4d4144
mthdht <mthdht@gmail.com>
2025-05-09 11:16:36 +0200

    refactor: move transition and placement mapping into utils

--- src/components/FloatingContent.vue
+++ src/components/FloatingContent.vue
@@ -14,6 +14,8 @@
 <script setup>
 import { inject, computed } from 'vue'
 
+import { getPlacementClass, getTransitionName } from '@/components/floating/utils.js'
+
 const dropdown = inject('dropdown')
 const pharos = inject('pharos')
 
@@ -21,52 +23,21 @@ const themeClasses = computed(() => {
     return pharos.getThemeClasses(props, 'dropdown')
 })
 
-const placementClasses = computed(() => {
-  const map = {
-    'top': 'bottom-full mb-2 left-1/2 -translate-x-1/2',
-    'bottom': 'top-full mt-2 left-1/2 -translate-x-1/2',
-    'left': 'right-full mr-2 top-1/2 -translate-y-1/2',
-    'right': 'left-full ml-2 top-1/2 -translate-y-1/2',
-
-    'top-start': 'bottom-full mb-2 left-0',
-    'top-end': 'bottom-full mb-2 right-0',
-    'bottom-start': 'top-full mt-2 left-0',
-    'bottom-end': 'top-full mt-2 right-0',
-
-    'left-start': 'right-full mr-2 top-0',
-    'left-end': 'right-full mr-2 bottom-0',
-    'right-start': 'left-full ml-2 top-0',
-    'right-end': 'left-full ml-2 bottom-0'
-  }
-  return map[dropdown.props.placement] || ''
-})
-
-// Transition name based on placement
-const transitionName = computed(() => {
-  const base = 'fade'
-  const map = {
-    top: 'fade-slide-down',
-    bottom: 'fade-slide-up',
-    left: 'fade-slide-right',
-    right: 'fade-slide-left',
-    'top-start': 'fade-slide-down',
-    'top-end': 'fade-slide-down',
-    'bottom-start': 'fade-slide-up',
-    'bottom-end': 'fade-slide-up',
-    'left-start': 'fade-slide-right',
-    'left-end': 'fade-slide-right',
-    'right-start': 'fade-slide-left',
-    'right-end': 'fade-slide-left',
-  }
-  return map[dropdown.props.placement] || base
-})
+const placementClasses = computed(() => getPlacementClass(dropdown.props.placement))
+const transitionName = computed(() => getTransitionName(dropdown.props.placement))
 
 
 </script>
 
 <style scoped>
 .fade-slide-down-enter-active, 
-.fade-slide-down-leave-active {
+.fade-slide-down-leave-active,
+.fade-slide-up-enter-active, 
+.fade-slide-up-leave-active,
+.fade-slide-right-enter-active, 
+.fade-slide-right-leave-active,
+.fade-slide-left-enter-active, 
+.fade-slide-left-leave-active {
   transition: all 0.3s ease;
 }
 
@@ -76,10 +47,7 @@ const transitionName = computed(() => {
   transform: translateY(20px);
 }
 
-.fade-slide-up-enter-active, 
-.fade-slide-up-leave-active {
-  transition: all 0.3s ease;
-}
+
 
 .fade-slide-up-enter-from, 
 .fade-slide-up-leave-to {
@@ -87,20 +55,10 @@ const transitionName = computed(() => {
   transform: translateY(-20px);
 }
 
-.fade-slide-right-enter-active, 
-.fade-slide-right-leave-active {
-  transition: all 0.3s ease;
-}
-
 .fade-slide-right-enter-from, 
 .fade-slide-right-leave-to {
   opacity: 0;
   transform: translateX(20px);
-}
-
-.fade-slide-left-enter-active, 
-.fade-slide-left-leave-active {
-  transition: all 0.3s ease;
 }
 
 .fade-slide-left-enter-from, 
