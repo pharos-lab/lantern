4e1990806278b71a768ff2033ae866ab1088123b
mthdht <mthdht@gmail.com>
2025-05-12 12:54:49 +0200

    add transition to overlay for dialog

--- src/components/DialogContent.vue
+++ src/components/DialogContent.vue
@@ -1,14 +1,30 @@
 <template>
-    <div v-if="dialog.isOpen.value" class="l-dialog fixed inset-0 bg-black/50 z-50 flex items-center justify-center m-0">
-        <div class="bg-white rounded-lg shadow-lg p-6">
-            <slot></slot>
+    <Transition>
+        <div v-if="dialog.isOpen.value" class="l-dialog-overlay fixed inset-0 bg-black/50 z-50 flex items-center justify-center m-0">
+            <div class="l-dialog-content bg-white rounded" :class="pharos.theme.padding">
+                <slot></slot>
+            </div>
         </div>
-    </div>
+    </Transition>
 </template>
   
 <script setup>
 import { inject } from 'vue'
 
 const dialog = inject('dialog')  
+
+const pharos = inject('pharos')
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
   
