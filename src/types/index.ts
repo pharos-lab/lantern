b6a2d3154a6cf5b08a7324be7480d5154298d5a3
mth <mthdht@gmail.com>
2025-11-14 08:43:48 +0000

    theme: made theme type

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface Theme {
+  variants: {
+    [variantName: string]: {
+      [key: string]: string; // n'importe quelle clé avec des classes Tailwind
+    };
+  };
+  
+  size: {
+    [sizeName: string]: string;
+  };
+  
+  radius: {
+    [radiusName: string]: string;
+  };
+  
+  components: {
+    [componentName: string]: {
+      variant: string; // default variant name
+      apply: string[]; // key list to apply from variant
+      class?: string;  // base classes
+      defaultProps?: {
+        size?: string;
+        radius?: string;
+      };
+      variants?: {
+        [variantName: string]: {
+          [key: string]: string; // override partiel
+        };
+      };
+      props?: {
+        size?: {
+          [sizeName: string]: string;
+        };
+        radius?: {
+          [radiusName: string]: string;
+        };
+      };
+    };
+  };
+}
